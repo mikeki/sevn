cb29dec896d5d5637b21cf3652459d7baf5fab3d
Miguel Cervera <miguelcerverac@gmail.com>
2015-03-29 23:23:00 -0700

    try to change codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,7 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 require 'simplecov'
 require 'coveralls'
-require 'rubygems'
-require 'bundler'
+require 'sevn'
+require 'codeclimate-test-reporter'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
@@ -14,10 +11,9 @@ SimpleCov.start do
   add_filter '/spec/'
 end
 
-require 'sevn'
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 Coveralls.wear!
+CodeClimate::TestReporter.start
